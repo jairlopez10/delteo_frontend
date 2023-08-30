ca2b934f4b26fbc6c348c1e4131ded8db6aa91c8
Jair Lopez <lopezarias7@hotmail.com>
2023-08-30 02:36:40 -0500

    Agregar boton WA contactarse

--- src/paginas/Nosotros.jsx
+++ src/paginas/Nosotros.jsx
@@ -25,7 +25,9 @@ const Nosotros = () => {
           <div className="p-contactar">
             <p>¡Bienvenidos a la Juguetería Jammy, donde todos los sueños se hacen realidad y cada sonrisa cuenta. Somos dos ingenieros industriales de la Pontificia Universidad Javeriana, los cuales buscamos brindar una excelente experiencia a todos los niños de nuestros seres queridos o amigos por medio de nuestros juguetes. Nos caracterizamos por ofrecer productos innovadores, de calidad, seguros y asequibles que quizás son inimaginables para nosotros y nos brindan una sonrisa. Realizamos envíos a cualquier parte del país, por lo que nos puedes contar cuál es el sueño de tu pequeño por cumplir!</p>
             <div className="adicion">
-              <div className="boton-ordenar boton-nosotros">
+              <div className="boton-ordenar boton-nosotros" onClick={() => {
+                window.open("https://wa.me/573246767492?text=Hola%20Jammy,%20quisiera%20saber%20mas%20informacion%20por%20favor%20", '_blank');
+              }}>
                 <img className="" src="/wa.webp" alt="wa" />
                 <p>CONTACTARSE</p>
               </div>
