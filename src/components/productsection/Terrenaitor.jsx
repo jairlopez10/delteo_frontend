24efd5da882c616622c094dfbed85387d5f0cc30
Jair Lopez <lopezarias7@hotmail.com>
2024-09-07 15:59:24 -0500

    Agregar pixel nuevo

--- src/components/productsection/Terrenaitor.jsx
+++ src/components/productsection/Terrenaitor.jsx
@@ -4,14 +4,19 @@ const Terrenaitor = () => {
     <>
         <div className="div-aida">
             <img src="/terrenaitor2.gif" alt="gif carro manilla" />
-            <h4 className=" mt-4">Domina cualquier terreno el Terrenaitor</h4>
-            <p className=" mt-4 text-center">¡El carro de control remoto más increíble con luces y vapor iluminado!</p>
+            <h4 className=" mt-4">¿Quieres dominar cualquier terreno y horario?</h4>
+            <p className=" mt-4 text-center">¡El carro de control remoto más increíble con <span className=" font-bold">luces</span>, vapor de agua  y totalmente recargable!</p>
         </div>
         <div className="div-aida">
           <img src="/terrenaitor1.gif" alt="gif carro manilla" />
-          <h4 className=" mt-4">El carro de control más emocionante</h4>
-          <p className=" mt-4 text-center">Maneja la acción con el control remoto o una experiencia más divertida usando la manilla con sensor</p>
+          <h4 className=" mt-4">¡Control con sensor para una mayor diversion!</h4>
+          <p className=" mt-4 text-center">Manejalo con la manilla sensor o su control remoto para diversion pura todo el dia!</p>
         </div>
+        <div className="div-aida">
+          
+          <iframe src="https://giphy.com/embed/3cRTDF4flJSxKmjFaw" className=" w-full" ></iframe>
+        </div>
+        
     </>
   )
 }
