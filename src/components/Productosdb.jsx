0d35308f404b16db31f3ff618af02d1c30a6afff
Jair Lopez <lopezarias7@hotmail.com>
2024-10-14 14:56:49 -0500

    Cambiar precio

--- src/components/Productosdb.jsx
+++ src/components/Productosdb.jsx
@@ -51,7 +51,7 @@ const productosdb = [
         id: 380,
         titulo: "Velociraptor Dinosaurio a Control y Vapor",
         preciomayorista: 74900,
-        precio: 129900,
+        precio: 79900,
         descripcion: '',
         imagenes: [
             {
