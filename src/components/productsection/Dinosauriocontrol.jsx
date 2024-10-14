027b56f308113e0f541c79634be0ddfe2e0a1bf6
Jair Lopez <lopezarias7@hotmail.com>
2024-10-14 15:17:21 -0500

    Actualizar dino

--- src/components/productsection/Dinosauriocontrol.jsx
+++ src/components/productsection/Dinosauriocontrol.jsx
@@ -3,28 +3,29 @@ const Dinosauriocontrol = () => {
   return (
     <>
         <div className="div-aida">
-            <img src="/dinocontrol2.gif" alt="gif carro manilla" />
-            <h4 className=" mt-4">¿Buscas una aventura prehistorica?</h4>
-            <p className=" mt-4 text-center">¡Tu hijo no resistira la emocion de ver como su Velociraptor <span className=" font-bold">lanza vapor</span> por la boca simulando fuego🔥!</p>
-        </div>
-        <div className="div-aida">
             <img src="/dinocontrol1.gif" alt="gif carro manilla" />
             <h4 className=" mt-4">¡Controla todos sus increibles movimientos!</h4>
             <p className=" mt-4 text-center">¡Utiliza su <span className="font-bold">control remoto</span> para manejar sus rugidos, pasos hacia adelante, atras o para lanzar fuego!</p>
         </div>
         <div className="div-aida">
-            <img src="/dinocontrol3.gif" alt="gif carro manilla" />
-            <h4 className=" mt-4">¡Olvidate de las pilas desechables!</h4>
-            <p className=" mt-4 text-center">¡Permite a tu hijo jugar hasta <span className="font-bold">2 horas</span> debido a su bateria recargable!</p>
+            <img src="/dinocontrol2.gif" alt="gif carro manilla" />
+            <h4 className=" mt-4">¿Buscas una aventura prehistorica?</h4>
+            <p className=" mt-4 text-center">¡Tu hijo no resistira la emocion de ver como su Velociraptor <span className=" font-bold">lanza vapor</span> por la boca simulando fuego🔥!</p>
         </div>
         <div className="div-aida">
-            <img src="/dinocontrol4.gif" alt="gif carro manilla" />
+            <img src="/dinocontrol3.gif" alt="gif carro manilla" />
+            <h4 className=" mt-4">¡Olvidate de las pilas desechables!</h4>
+            <p className=" mt-4 text-center">¡Permite a tu hijo jugar debido a su <span className="font-bold">bateria recargable</span> para hasta 2 horas continuas!</p>
+        </div>
+        <div className="div-aida">
+            
             <h4 className=" mt-4">¿Qué es lo que incluye?</h4>
             <div className="lista-incluye">
                 <ul >
                     <li>Velociraptor Dinosaurio</li>
                     <li>Control remoto</li>
                     <li>Bateria Recargable</li>
+                    <li>Lanza Vapor de Agua</li>
                     <li>Cargador USB</li>
                     <li>Dispensador agua</li>
                 </ul>
