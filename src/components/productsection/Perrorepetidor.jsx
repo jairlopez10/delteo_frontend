a456e01d7f9ab5a22b68749febb35d757f553330
Jair Lopez <lopezarias7@hotmail.com>
2024-12-09 22:23:58 -0500

    Subir perro

--- src/components/productsection/Perrorepetidor.jsx
+++ src/components/productsection/Perrorepetidor.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Perrorepetidor = () => {
   return (
     <>
-        <h4 className=" mt-8">Te escriberemos para confirmar el motivo 🐕</h4>
+        <h4 className=" mt-8">¡Te escriberemos para confirmar el motivo! (Blanco o Cafe) 🐕</h4>
         <div className="div-aida">
             <img src="/perrogif1.gif" alt="gif ak" />
             <h4 className=" mt-4">¿Tus hij@s sueñan con tener una mascota?</h4>
@@ -15,21 +15,20 @@ const Perrorepetidor = () => {
             <p className=" mt-4 text-center">¡Camina y ladra cuando lo <span className=' font-bold'>concientes</span> en su cabeza o su cuerpo!</p>
         </div>
         <div className="div-aida">
-            <img src="/producto2cgif.gif" alt="gif ak" />
-            <h4 className=" mt-4">¡Llevalo a todas partes!</h4>
-            <p className=" mt-4 text-center">¡Su funcionamiento con <span className='font-bold'>3 pilas AA </span>  te permite llevarlo a cualquier lugar para nunca dejar de aprender!</p>
+            <img src="/perrogif3.gif" alt="gif ak" />
+            <h4 className=" mt-4">¡Repite todas las aventuras que le cuentes!</h4>
+            <p className=" mt-4 text-center">¡Hablale y <span className='font-bold'>cuentale una historia</span> y la repetira completa! - Bateria Recargable 🔋 </p>
         </div>
 
         <div className="div-aida">
           <h4 className=" mt-4">¿Qué es lo que incluye?</h4>
           <div className="lista-incluye">
             <ul >
-              <li>Computador Interactivo</li>
-              <li>Mouse</li>
-              <li>+20 Actividades <span className='font-bold'>Matematicas</span></li>
-              <li>+10 Actividades en <span className='font-bold'>Ingles</span> </li>
-              <li>+10 Actividades de <span className='font-bold'>Letras</span> </li>
-              <li>+10 Actividades de <span className='font-bold'>Ortografia</span> </li>
+              <li>Perro Sensor Interactivo</li>
+              <li>Lazo</li>
+              <li>Bateria Recargable 🔋</li>
+              <li>Cable USB</li>
+              <li>Collar o pañuelo</li>
             </ul>
           </div>
         </div>
