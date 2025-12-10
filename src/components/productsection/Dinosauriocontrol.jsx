b57b388d791fbf450257076d694a07d6d21f047d
Jair Lopez <lopezarias7@hotmail.com>
2025-12-09 21:26:03 -0500

    Actualizar pagina dino

--- src/components/productsection/Dinosauriocontrol.jsx
+++ src/components/productsection/Dinosauriocontrol.jsx
@@ -3,14 +3,14 @@ const Dinosauriocontrol = () => {
   return (
     <>
         <div className="div-aida">
-            <img src="/dinocontrol1.gif" alt="gif carro manilla" />
-            <h4 className=" mt-4">¡Controla todos sus increibles movimientos!</h4>
-            <p className=" mt-4 text-center">¡Utiliza su <span className="font-bold">control remoto</span> para manejar sus rugidos, pasos hacia adelante, atras o para lanzar fuego!</p>
-        </div>
-        <div className="div-aida">
             <img src="/dinocontrol2.gif" alt="gif carro manilla" />
             <h4 className=" mt-4">¿Buscas una aventura prehistorica?</h4>
             <p className=" mt-4 text-center">¡Tu hijo no resistira la emocion de ver como su Velociraptor <span className=" font-bold">lanza vapor</span> por la boca simulando fuego🔥!</p>
+        </div>
+        <div className="div-aida">
+            <img src="/dinocontrol1.gif" alt="gif carro manilla" />
+            <h4 className=" mt-4">¡Controla todos sus increibles movimientos!</h4>
+            <p className=" mt-4 text-center">¡Utiliza su <span className="font-bold">control remoto</span> para manejar sus rugidos, pasos hacia adelante, atras o para lanzar fuego!</p>
         </div>
         <div className="div-aida">
             <img src="/dinocontrol3.gif" alt="gif carro manilla" />
